a49cabf515808cb0f1108623e39219a944a2544e
Tilahun-git <tilahuntareke8@gmail.com>
2026-07-16 23:03:06 +0300

    add governance service policy entity and enums

---
+++ governance-service/src/main/java/com/internship/governanceservice/entity/Policy.java
@@ -0,0 +1,38 @@
+package com.internship.governanceservice.entity;
+
+
+import com.internship.governanceservice.enums.PolicyStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "policies")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Policy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 2000)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PolicyStatus status;
+
+    @Column(name = "created_by", nullable = false)
+    private String createdBy;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+}
---
+++ governance-service/src/main/java/com/internship/governanceservice/enums/PolicyStatus.java
@@ -0,0 +1,8 @@
+package com.internship.governanceservice.enums;
+
+public enum PolicyStatus {
+    DRAFT,
+    PENDING_APPROVAL,
+    APPROVED,
+    REJECTED,
+}
