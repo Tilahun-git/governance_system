958c4908acd0b2b2bb265f7fdf0b305b7a8bfb57
Tilahun-git <tilahuntareke8@gmail.com>
2026-07-31 10:23:33 +0300

    update the mapper from manual mapping to using mapStruct

--- governance-service/src/main/java/com/internship/governanceservice/mapper/PolicyMapper.java
+++ governance-service/src/main/java/com/internship/governanceservice/mapper/PolicyMapper.java
@@ -4,30 +4,20 @@ import com.internship.governanceservice.dto.request.CreatePolicyRequest;
 import com.internship.governanceservice.dto.response.PolicyResponse;
 import com.internship.governanceservice.entity.Policy;
 import com.internship.governanceservice.enums.PolicyStatus;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.time.LocalDateTime;
-@Component
-public class PolicyMapper {
-    public Policy mapToPolicyEntity(CreatePolicyRequest createPolicyDto) {
-        return Policy.builder()
-                .title(createPolicyDto.getTitle())
-                .description(createPolicyDto.getDescription())
-                .createdBy(createPolicyDto.getCreatedBy())
-                .createdAt(LocalDateTime.now())
-                .status(PolicyStatus.DRAFT)
-                .build();
-    }
 
-    public PolicyResponse mapToPolicyResponseDto(Policy policy) {
-        return PolicyResponse.builder()
-                .id(policy.getId())
-                .title(policy.getTitle())
-                .description(policy.getDescription())
-                .status(policy.getStatus())
-                .createdBy(policy.getCreatedBy())
-                .createdAt(policy.getCreatedAt())
-                .build();
-    }
+@Mapper(componentModel = "spring",
+        imports = {LocalDateTime.class, PolicyStatus.class})
+public interface PolicyMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", expression = "java(LocalDateTime.now())")
+    @Mapping(target = "status", expression = "java(PolicyStatus.DRAFT)")
+    Policy mapToPolicyEntity(CreatePolicyRequest request);
+
+    PolicyResponse mapToPolicyResponseDto(Policy policy);
 
 }
